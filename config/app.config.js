c7dd63e797f1dc82cb758b6e986f5ef977b7e838
tanzilahuda <tanzilahuda99@gmail.com>
2021-03-16 17:57:14 +0530

    Update app.config.js

--- config/app.config.js
+++ config/app.config.js
@@ -1,6 +1,6 @@
 exports.config={
     PORT:13899,
-   //MONGO_URL: "mongodb+srv://sai:sai@ecommerce.obdec.mongodb.net/7amecommerce?retryWrites=true&w=majority",
+   MONGO_URL: "mongodb+srv://tanzilahuda:<hudasiddiqua99>@cluster0.56vsv.mongodb.net/mongDB1?retryWrites=true&w=majority",
    MONGO_URL: "mongodb://localhost/cws",
     JWT_SECRET:'bdkvkdbkdddjdbv'
 }
